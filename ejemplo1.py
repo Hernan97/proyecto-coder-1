25d83c5898465f3e2629cac671450ada566c602c
Hernan Cordella <hernancordella@gmail.com>
2024-07-25 20:41:41 -0300

    Agrego multiplicacion a ejemplo1 y division al ejemplo2

--- ejemplo1.py
+++ ejemplo1.py
@@ -7,3 +7,9 @@ def sumar(n1,n2):
     print("El resultado de la suma entre n1 y n2 es de: ", n1+n2)
 
 sumar(int(input("Ingrese el primer numero: ")), int((input("Ingrese el segundo numero: "))))
+
+
+def multiplicar(n1,n2):
+    print("El resultado de la suma entre n1 y n2 es de: ", n1*n2)
+
+multiplicar(int(input("Ingrese el primer numero: ")), int((input("Ingrese el segundo numero: "))))
